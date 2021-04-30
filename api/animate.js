3e2b9c2a079029c500b65fabef4256aef7cb54fe
Vignesh H <meeravignesh2011@gmail.com>
2021-04-30 11:54:46 +0530

    Home page commit

--- api/animate.js
+++ api/animate.js
@@ -1,3 +1,13 @@
+var aa = {};
+aa.scaleIn = [0.2, 1];
+aa.scaleOut = 3;
+aa.durationIn = 1000;
+aa.delay = 1000;
+
+var textWrapper = document.querySelector('.slogan');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
 anime.timeline({loop: false})
   .add({
     targets: '.anitext',
@@ -8,10 +18,43 @@ anime.timeline({loop: false})
     delay: (el, i) => 800 * i
   })
   .add({targets:'.animation', margin:0},0)
- // .add({
- //   targets: '.anitext',
- //  translateY: -250,
- //   easing: 'easeInOutSine'
- // })
   .add({targets:'.anitext', color:'#c3073f'},0)
+  .add({
+    targets: '.slogan .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 300,
+    delay: (el, i) => 80 * (i+1)
+  })
   
+  .add({
+    targets: '.homefaculty',
+    opacity: [0,1],
+    scale: aa.scaleIn,
+    duration: aa.durationIn
+  })
+  .add({
+    targets: '.homeadmin',
+    opacity: [0,1],
+    scale: aa.scaleIn,
+    duration: aa.durationIn
+  })
+  .add({
+    targets: '.homedean',
+    opacity: [0,1],
+    scale: aa.scaleIn,
+    duration: aa.durationIn
+  })
+  .add({
+    targets: '.helloani',
+    opacity: [0,1],
+    scale: aa.scaleIn,
+    duration: aa.durationIn
+  });
+  
+
+
+  
+  
+
+
